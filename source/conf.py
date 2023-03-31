69d79781c793851b0da4d089bdd1824d5af2291f
Frank Cichos <cichos@physik.uni-leipzig.de>
2023-03-31 11:29:34 +0200

    new style test

--- source/conf.py
+++ source/conf.py
@@ -57,7 +57,7 @@ mathjax3_config = {
 
 #html_theme = 'sphinx_rtd_theme'
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 
 #def setup(app):
 #    app.add_stylesheet('theme_overrides.css')
