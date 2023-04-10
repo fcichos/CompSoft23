710508bacff31118a8cdcff8931510f47b88af64
Frank Cichos <cichos@physik.uni-leipzig.de>
2023-04-10 21:23:57 +0200

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -118,7 +118,7 @@ nbsphinx_prolog = r"""
 
     .. nbinfo::
         This page was generated from `{{ docname }}`.        
-        :raw-html:`<br/><a href="https://colab.research.google.com/github/fcichos/CompSoft22/blob/master/source/{{ docname }}"><img alt="Binder badge" src="https://img.shields.io/badge/launch-%20colab-green.svg" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<br/><a href="https://colab.research.google.com/github/fcichos/CompSoft23/blob/master/source/{{ docname }}"><img alt="Binder badge" src="https://img.shields.io/badge/launch-%20colab-green.svg" style="vertical-align:text-bottom"></a>`
         :raw-html:`<br/><a href="https://mybinder.org/v2/gh/fcichos/CompSoft23/master?urlpath=tree/source/{{ docname }}"><img alt="Binder badge" src="https://img.shields.io/badge/launch-%20myBinder-red.svg" style="vertical-align:text-bottom"></a>` 
         
 
